b0ca69e5a0259091f23d29772ca5b4115eb0d15b
Xavier Damman <xdamman@gmail.com>
2013-11-17 00:05:07 -0800

    Loading livereload with requirejs

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,1 +1,16 @@
+ENV = (window.location.port) ? "development" : "production";
+
+require.config({
+  baseUrl: "/public/js/",
+  paths: {
+  }
+});
+
+var scripts = {
+  "production" : [],
+  "development" : ["livereload"]
+};
+
+require(scripts[ENV]);
+
 console.log("Hello world from main.js");
